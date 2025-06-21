5d4a7efcee6fcdfd56b42ce6055ca84675599396
SzymonTokarzProgramista <149623510+SzymonTokarzProgramista@users.noreply.github.com>
2025-06-21 11:10:56 +0200

    init

---
+++ parameters.m
@@ -0,0 +1,23 @@
+%parameters
+g=9.8036; %m/s^2
+m=0.4; %kg
+rmin=0.1; %m
+rmax=0.4; %m
+theta_min = -pi/2;%rad
+theta_max=pi/2;%rad
+dtheta_min=-10;%rad/s
+dtheta_max=10;%rad/s
+dr_min=-10;%m/s
+dr_max=10;%m/s
+b1=0.2;
+b2=1;
+
+
+%initial conditions
+theta_start = pi/6;%rad
+r_start = 0.2; %m
+
+
+%desired
+r_desired = 0.3;
+theta_desired = pi/2;
